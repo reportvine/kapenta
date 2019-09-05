b60027cd393f626f5f515aea68a2c24766f931df
Zikani Nyirenda Mwase <zikani@creditdatamw.com>
2019-09-05 12:24:56 +0200

    Break PentahoApiGenerator into separate classes
    Moved functionality to separate classes to make adding tests easier
    also too modularize the code

--- src/main/java/com/creditdatamw/labs/sparkpentaho/autogen/PentahoApiGenerator.java
+++ src/main/java/com/creditdatamw/labs/sparkpentaho/autogen/PentahoApiGenerator.java
@@ -1,35 +1,20 @@
 package com.creditdatamw.labs.sparkpentaho.autogen;
 
-import com.creditdatamw.labs.sparkpentaho.SparkPentahoAPI;
 import com.creditdatamw.labs.sparkpentaho.config.ApiConfiguration;
 import com.creditdatamw.labs.sparkpentaho.config.Method;
 import com.creditdatamw.labs.sparkpentaho.config.ReportConfiguration;
 import com.creditdatamw.labs.sparkpentaho.reports.OutputType;
-import com.creditdatamw.labs.sparkpentaho.reports.ParameterDefinition;
-import com.creditdatamw.labs.sparkpentaho.reports.ReportDefinition;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 import org.pentaho.reporting.engine.classic.core.ClassicEngineBoot;
-import org.pentaho.reporting.engine.classic.core.MasterReport;
-import org.pentaho.reporting.engine.classic.core.parameters.DefaultParameterContext;
-import org.pentaho.reporting.engine.classic.core.parameters.ParameterDefinitionEntry;
-import org.pentaho.reporting.libraries.resourceloader.Resource;
 import org.pentaho.reporting.libraries.resourceloader.ResourceManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.io.OutputStream;
-import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
@@ -45,9 +30,8 @@ import java.util.stream.Collectors;
 public class PentahoApiGenerator {
     private static final Logger LOGGER = LoggerFactory.getLogger(PentahoApiGenerator.class);
     private static final YAMLMapper yamlMapper = new YAMLMapper();
-    private static final ObjectMapper objectMapper = SparkPentahoAPI.OBJECT_MAPPER;
     private final ResourceManager resourceManager;
-
+    private ReportDefinitionDirectoryWalker directoryWalker;
     private final Path sourceDir;
     private final Path outputFile;
 
@@ -63,11 +47,15 @@ public class PentahoApiGenerator {
         ClassicEngineBoot.getInstance().start();
         this.resourceManager = new ResourceManager();
         resourceManager.registerDefaults();
+        this.directoryWalker = new ReportDefinitionDirectoryWalker(
+            this.sourceDir,
+            -1,
+            this.resourceManager
+        );
     }
 
-    public void generate() throws IOException {
-        List<ReportDefinition> defs = searchForPrpts(sourceDir);
-        ApiConfiguration apiConfiguration = buildConfiguration(defs);
+    public void generate() {
+        ApiConfiguration apiConfiguration = buildConfiguration();
 
         Objects.requireNonNull(apiConfiguration);
 
@@ -81,134 +69,41 @@ public class PentahoApiGenerator {
         }
     }
 
-    private ApiConfiguration buildConfiguration(List<ReportDefinition> reportDefinitions) {
+    private ApiConfiguration buildConfiguration() {
         ApiConfiguration apiConfiguration = new ApiConfiguration();
 
         apiConfiguration.setApiRoot("/api");
         apiConfiguration.setHost("localhost");
         apiConfiguration.setPort(4567);
-        apiConfiguration.setReports(reportDefinitions.stream()
-                .map(reportDefinition -> {
-                    ReportConfiguration reportConfiguration = new ReportConfiguration();
+        apiConfiguration.setReports(directoryWalker.getReportDefinitionStream()
+            .map(reportDefinition -> {
+                ReportConfiguration reportConfiguration = new ReportConfiguration();
 
-                    String reportPath = reportDefinition.getReportName().toLowerCase().replace(" ", "_");
-                    
-                    reportConfiguration.setPath(reportPath.startsWith("/") ? reportPath : "/".concat(reportPath));
+                String reportPath = reportDefinition.getReportName().toLowerCase().replace(" ", "_");
 
-                    reportConfiguration.setReportName(reportDefinition.getReportName());
-                    reportConfiguration.setDescription(reportDefinition.getDescription());
-                    reportConfiguration.setVersion(reportDefinition.getVersion());
-                    reportConfiguration.setParameters(reportDefinition.getParameters());
-                    reportConfiguration.setReportFilePath(reportDefinition.getReportFilePath());
-                    
-                    // Support both GET and POST by default
-                    reportConfiguration.setMethods(Method.ALL);
+                reportConfiguration.setPath(reportPath.startsWith("/") ? reportPath : "/".concat(reportPath));
 
-                    // Add all the output types for this report
-                    reportConfiguration.setExtensions(Arrays.stream(OutputType.values())
-                        .filter(o -> !o.equals(OutputType.NONE))
-                        .map(OutputType::name)
-                        .collect(Collectors.toList()));
+                reportConfiguration.setReportName(reportDefinition.getReportName());
+                reportConfiguration.setDescription(reportDefinition.getDescription());
+                reportConfiguration.setVersion(reportDefinition.getVersion());
+                reportConfiguration.setParameters(reportDefinition.getParameters());
+                reportConfiguration.setReportFilePath(reportDefinition.getReportFilePath());
 
-                    return reportConfiguration;
-                })
-                .collect(Collectors.toList()));
+                // Support both GET and POST by default
+                reportConfiguration.setMethods(Method.ALL);
+
+                // Add all the output types for this report
+                reportConfiguration.setExtensions(Arrays.stream(OutputType.values())
+                    .filter(o -> !o.equals(OutputType.NONE))
+                    .map(OutputType::name)
+                    .collect(Collectors.toList()));
+
+                return reportConfiguration;
+            })
+            .collect(Collectors.toList()));
 
         return apiConfiguration;
     }
 
-    /**
-     * Search for pentaho report files in the given directory
-     * 
-     * @return a list of the absolute paths to the pentaho files
-     */
-    private List<ReportDefinition> searchForPrpts(Path filePath) {
-        try {
-            final List<ReportDefinition> prptFiles = new ArrayList<>();
-            Files.walkFileTree(filePath, new SimpleFileVisitor<Path>() {
-                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                    if (file.getFileName().toString().endsWith(".prpt")) {
-                        try {
-                            ReportDefinition reportDefinition = parseReportDefinition(file);
-                            prptFiles.add(reportDefinition);
-                        } catch(Exception ex) {
-                            LOGGER.error("Failed to parse report file: " + file, ex);
-                        }
-                    }
-                    return FileVisitResult.CONTINUE;
-                }
-            });
-            return prptFiles;
-        } catch(IOException ioe) {
-            LOGGER.error("Failed to search for prpts in : " + filePath, ioe);
-        }
-        return Collections.emptyList();
-    }
 
-    private ReportDefinition parseReportDefinition(final Path reportFilePath) throws Exception {
-        Objects.requireNonNull(reportFilePath, "reportFilePath cannot be null");
-
-        final Resource resource = resourceManager.createDirectly(reportFilePath.toFile(), MasterReport.class);
-
-        final MasterReport pentahoReport = (MasterReport) resource.getResource();
-
-        List<ParameterDefinitionEntry> pentahoParams = Arrays.asList(pentahoReport.getParameterDefinition()
-                                                                                  .getParameterDefinitions());
-        List<ParameterDefinition> params = pentahoParams.stream()
-                .map(param -> mapPentahoParameterToParameterDefinition(param, pentahoReport))
-                .filter(p -> !Objects.isNull(p))
-                .collect(Collectors.toList());
-
-        // Ensure that all the parameters are extracted
-        if (pentahoParams.size() != params.size()) {
-            String realParams = objectMapper.writeValueAsString(pentahoParams);
-            String extractedParams = objectMapper.writeValueAsString(params);
-            String comparison = String.format("Expected: %s, Actual: %s", realParams, extractedParams);
-            throw new Exception("Parameters in report definition and extracted parameters do not match. " + comparison);
-        }
-
-        if (Objects.isNull(reportFilePath.getFileName())) {
-            throw new Exception("Failed to read fileName from reportFilePath");
-        }
-        String reportName = reportFilePath.getFileName().toString();
-        // Remove the extension
-        reportName = reportName.replace(".prpt", "");
-        // TODO: Get version and description from report metadata
-        String version = pentahoReport.getId();
-        String description = pentahoReport.getTitle();
-        
-        return new ReportDefinition(reportName, reportFilePath.toAbsolutePath().toString(), params, version, description);
-    }
-
-    /**
-     * Map a pentaho report parameter to the ParameterDefinition class
-     *
-     * @param param
-     * @param pentahoReport
-     * @return
-     */
-    private ParameterDefinition mapPentahoParameterToParameterDefinition(ParameterDefinitionEntry param, MasterReport pentahoReport) {
-        Object defaultValue = null;
-        try {
-            /**
-             * Pentaho requires us to provide a {@link org.pentaho.reporting.engine.classic.core.parameters.ParameterContext}
-             * which may fail since it needs to connect to the DataFactory (database)
-             * for some other kinds of parameters so we allow the call to fail in which case
-             * the default will remain a <code>null</code>
-             */
-            DefaultParameterContext context = new DefaultParameterContext(pentahoReport);
-            defaultValue = param.getDefaultValue(context);
-        } catch(Exception ex) {
-            LOGGER.error("Failed to get default value for parameter", ex);
-        }
-
-        if (param.isMandatory()) {
-            return ReportDefinition.requiredParameter(param.getName(),
-                    defaultValue,
-                    param.getValueType());
-        }
-        return ReportDefinition.optionalParameter(param.getName(),
-                defaultValue,
-                param.getValueType());
-    }
 }
---
+++ src/main/java/com/creditdatamw/labs/sparkpentaho/autogen/ReportDefinitionFileReader.java
@@ -0,0 +1,67 @@
+package com.creditdatamw.labs.sparkpentaho.autogen;
+
+import com.creditdatamw.labs.sparkpentaho.SparkPentahoAPI;
+import com.creditdatamw.labs.sparkpentaho.parameters.PentahoToParameterDefinitionMapper;
+import com.creditdatamw.labs.sparkpentaho.reports.ParameterDefinition;
+import com.creditdatamw.labs.sparkpentaho.reports.ReportDefinition;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.pentaho.reporting.engine.classic.core.MasterReport;
+import org.pentaho.reporting.engine.classic.core.parameters.ParameterDefinitionEntry;
+import org.pentaho.reporting.libraries.resourceloader.Resource;
+import org.pentaho.reporting.libraries.resourceloader.ResourceManager;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class ReportDefinitionFileReader {
+    private static final ObjectMapper objectMapper = SparkPentahoAPI.OBJECT_MAPPER;
+    private final ResourceManager resourceManager;
+    private final PentahoToParameterDefinitionMapper parameterMapper =
+        new PentahoToParameterDefinitionMapper();
+
+    ReportDefinitionFileReader(ResourceManager resourceManager) {
+        this.resourceManager = Objects.requireNonNull(resourceManager);
+    }
+
+    public ReportDefinition createFromFile(final Path parentDir,
+                                           final Path reportFilePath,
+                                           boolean relativizeApiPath) throws Exception {
+
+        Objects.requireNonNull(reportFilePath, "reportFilePath cannot be null");
+
+        final Resource resource = resourceManager.createDirectly(reportFilePath.toFile(), MasterReport.class);
+
+        final MasterReport pentahoReport = (MasterReport) resource.getResource();
+
+        List<ParameterDefinitionEntry> pentahoParams = Arrays.asList(pentahoReport.getParameterDefinition()
+                .getParameterDefinitions());
+        List<ParameterDefinition> params = pentahoParams.stream()
+                .map(param -> parameterMapper.map(param, pentahoReport))
+                .filter(p -> !Objects.isNull(p))
+                .collect(Collectors.toList());
+
+        // Ensure that all the parameters are extracted
+        if (pentahoParams.size() != params.size()) {
+            String realParams = objectMapper.writeValueAsString(pentahoParams);
+            String extractedParams = objectMapper.writeValueAsString(params);
+            String comparison = String.format("Expected: %s, Actual: %s", realParams, extractedParams);
+            throw new Exception("Parameters in report definition and extracted parameters do not match. " + comparison);
+        }
+
+        if (Objects.isNull(reportFilePath.getFileName())) {
+            throw new Exception("Failed to read fileName from reportFilePath");
+        }
+        String reportName = reportFilePath.getFileName().toString();
+        // Remove the extension
+        reportName = reportName.replace(".prpt", "");
+        // TODO: Get version and description from report metadata
+        String version = pentahoReport.getId();
+        String description = pentahoReport.getTitle();
+
+        return new ReportDefinition(reportName, reportFilePath.toAbsolutePath().toString(), params, version, description);
+    }
+}

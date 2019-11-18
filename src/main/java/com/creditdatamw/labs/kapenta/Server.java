c3eb74e35837727e6a7e1dd0db44a00f1aa30810
Zikani Nyirenda Mwase <zikani@creditdatamw.com>
2019-11-18 15:29:25 +0200

    Configure logging and fix server construction

--- src/main/java/com/creditdatamw/labs/kapenta/Server.java
+++ src/main/java/com/creditdatamw/labs/kapenta/Server.java
@@ -2,6 +2,7 @@ package com.creditdatamw.labs.kapenta;
 
 import com.creditdatamw.labs.kapenta.config.ApiConfiguration;
 import com.creditdatamw.labs.kapenta.config.BasicAuth;
+import com.creditdatamw.labs.kapenta.config.LoggingConfiguration;
 import com.creditdatamw.labs.kapenta.config.Method;
 import com.creditdatamw.labs.kapenta.filter.BasicAuthenticationFilter;
 import com.creditdatamw.labs.kapenta.http.ReportResource;
@@ -13,13 +14,13 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import org.pentaho.reporting.engine.classic.core.ClassicEngineBoot;
 import org.slf4j.LoggerFactory;
 import spark.Service;
-import spark.Spark;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * Main API for creating APIs out of Pentaho .prpt generator via SparkJava
@@ -29,6 +30,8 @@ public class Server {
     public static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
     private ApiConfiguration configuration;
     private final Reports reports;
+    private spark.Service httpServer;
+    private CountDownLatch countDownLatch = new CountDownLatch(1);
 
     Server(String apiRoot, List<ReportResource> availableReports) {
         Objects.requireNonNull(apiRoot);
@@ -36,10 +39,12 @@ public class Server {
         reports = new Reports(apiRoot, availableReports);
     }
 
-    Server(String resourceDefinitionYaml) {
+    private Server(String resourceDefinitionYaml) {
         Objects.requireNonNull(resourceDefinitionYaml);
         configuration = createFromYaml(resourceDefinitionYaml);
+        this.configureLogging(configuration);
         Path yamlFileDir = Paths.get(resourceDefinitionYaml).getParent();
+        this.httpServer = createHttpServer();
         reports = createReportsFromConfiguration(yamlFileDir, configuration);
     }
 
@@ -49,11 +54,27 @@ public class Server {
 
     public void start() {
         ClassicEngineBoot.getInstance().start();
-        createHttpServer();
+
+        final String rootPath = configuration.getApiRoot();
+        httpServer.get(rootPath.concat("/").concat("reports.json"),
+                new ReportsRoute(reports));
+
+        reports.setHttpServer(httpServer);
+
+        // Registers Spark Routes for the Reports
+        reports.registerResources();
+
+        try {
+            httpServer.awaitStop();
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void stop() {
-        Spark.stop();
+        httpServer.stop();
+        countDownLatch.countDown();
     }
 
     /**
@@ -87,6 +108,9 @@ public class Server {
     }
 
     private Reports createReportsFromConfiguration(Path yamlFileDir, ApiConfiguration configuraiton) {
+        Objects.requireNonNull(configuraiton, "configuration");
+        Objects.requireNonNull(httpServer, "Server.httpServer");
+
         List<ReportResource> reportResources = new ArrayList<>();
 
         final String[] defaultMethods = new String[] { "GET", "POST" };
@@ -113,6 +137,7 @@ public class Server {
         if (Optional.ofNullable(configuration.getBasicAuth()).isPresent()) {
             configureBasicAuth(configuration);
         }
+
         final Reports reports = new Reports(configuration.getApiRoot(),
                            Collections.unmodifiableList(reportResources),
                            configuration.getBackup(),
@@ -122,39 +147,39 @@ public class Server {
         return reports;
     }
 
-    private void createHttpServer() {
-        final String rootPath = configuration.getApiRoot();
-
+    private spark.Service createHttpServer() {
         String host = Optional.ofNullable(configuration.getHost()).orElse("0.0.0.0");
-
-        spark.Service server = Service.ignite();
-
-        server.ipAddress(host);
-        server.port(configuration.getPort());
-        server.get(rootPath.concat("/").concat("reports.json"),new ReportsRoute(reports));
-
-        reports.setHttpServer(server);
-        // Registers Spark Routes for the Reports
-        reports.registerResources();
+        httpServer = Service.ignite();
+        httpServer.ipAddress(host);
+        httpServer.port(configuration.getPort());
+        return httpServer;
     }
 
-    private static void configureBasicAuth(ApiConfiguration configuration) {
+    private void configureBasicAuth(ApiConfiguration configuration) {
+        final List<BasicAuth.User> userList = new ArrayList<>();
+
         BasicAuth basicAuth = configuration.getBasicAuth();
-        boolean multipleUsers = false;
 
-        BasicAuth.User user = basicAuth.getUser();
-        List<BasicAuth.User> userList = basicAuth.getUsers();
+        Optional.ofNullable(basicAuth.getUser())
+                .ifPresent(userList::add);
 
-        if (! Objects.isNull(userList) && !userList.isEmpty()) {
-            multipleUsers = true;
+        Optional.ofNullable(basicAuth.getUsers())
+                .ifPresent(userList::addAll);
+
+        if (!userList.isEmpty()) {
+            httpServer.before(new BasicAuthenticationFilter(userList));
         }
+    }
 
-        if (multipleUsers) {
-            // Configure for multiple users
-            Spark.before(new BasicAuthenticationFilter(userList));
-        } else {
-            // Configure auth for the single user
-            Spark.before(new BasicAuthenticationFilter(user));
-        }
+    private void configureLogging(ApiConfiguration apiConfiguration) {
+
+        LoggingConfiguration log = Optional.ofNullable(apiConfiguration.getLogging())
+                .orElse(new LoggingConfiguration());
+
+        System.setProperty("logging.directory",
+                Optional.ofNullable(log.getDirectory()).orElse("./logs"));
+
+        System.setProperty("logging.rootLevel",
+                Optional.ofNullable(log.getLevel()).orElse("INFO"));
     }
 }

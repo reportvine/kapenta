ffc81dcf8aaaaa4787fab6478e077fa2ce9a8269
Zikani Nyirenda Mwase <zikani@creditdatamw.com>
2021-02-05 20:32:59 +0200

    Suppress report paths in /api/reports.json API response data

--- src/main/java/com/creditdatamw/labs/kapenta/http/ReportsRoute.java
+++ src/main/java/com/creditdatamw/labs/kapenta/http/ReportsRoute.java
@@ -1,28 +1,56 @@
 package com.creditdatamw.labs.kapenta.http;
 
+import com.creditdatamw.labs.kapenta.OutputType;
+import com.creditdatamw.labs.kapenta.reportdefinition.ReportDefinition;
 import org.slf4j.LoggerFactory;
 import spark.Request;
 import spark.Response;
 import spark.Route;
 
-import java.util.ArrayList;
+import java.util.EnumSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.creditdatamw.labs.kapenta.http.ReportRoute.APPLICATION_JSON;
 import static com.creditdatamw.labs.kapenta.http.ReportRoute.toJson;
 
 public class ReportsRoute implements Route {
-    private final Reports reports;
+    private final List<ReportResource> reportResources;
     public ReportsRoute(final Reports reports) {
-        this.reports = reports;
+        this.reportResources = removePaths(reports.resources());
     }
 
     @Override
-    public Object handle(Request request, Response response) throws Exception {
+    public Object handle(Request request, Response response) {
         LoggerFactory.getLogger(getClass()).debug("Processing ReportsRoute request");
-        List<ReportResource> reportResources = new ArrayList<>(reports.resources());
         response.status(200);
         response.type(APPLICATION_JSON);
         return toJson(reportResources);
+    }
+
+    private List<ReportResource> removePaths(List<ReportResource> reports) {
+        return reports.stream()
+            .map(r -> new ReportResource() {
+                @Override
+                public String path() {
+                    return null;
+                }
+
+                @Override
+                public ReportDefinition reportDefinition() {
+                    return r.reportDefinition();
+                }
+
+                @Override
+                public String[] methods() {
+                    return r.methods();
+                }
+
+                @Override
+                public EnumSet<OutputType> outputTypes() {
+                    return r.outputTypes();
+                }
+            })
+            .collect(Collectors.toList());
     }
 }

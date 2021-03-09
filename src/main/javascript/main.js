c03eac49c266b404139f423ea6ca8a4cd85a2a22
Zikani Nyirenda Mwase <zikani@creditdatamw.com>
2021-03-09 11:58:28 +0200

    Remove buefy, add bootstrap and bootstrap-vue dependencies

--- src/main/javascript/main.js
+++ src/main/javascript/main.js
@@ -1,7 +1,15 @@
-import Vue from 'vue';
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import VueRouter from 'vue-router';
 import App from './App.vue';
 import ReportsIndex from './components/ReportsIndex.vue';
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false;
 Vue.use(VueRouter)
